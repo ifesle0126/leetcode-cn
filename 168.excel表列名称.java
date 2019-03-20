46587ac73d7c3e0197b12f5ebad1eca6c870b38c
zhujq <zhujq@ifeng.com>
2019-03-20 11:35:51 +0800

    test

---
+++ 168.excel表列名称.java
@@ -0,0 +1,65 @@
+/*
+ * @lc app=leetcode.cn id=168 lang=java
+ *
+ * [168] Excel表列名称
+ *
+ * https://leetcode-cn.com/problems/excel-sheet-column-title/description/
+ *
+ * algorithms
+ * Easy (30.45%)
+ * Total Accepted:    5.9K
+ * Total Submissions: 19.3K
+ * Testcase Example:  '1'
+ *
+ * 给定一个正整数，返回它在 Excel 表中相对应的列名称。
+ * 
+ * 例如，
+ * 
+ * ⁠   1 -> A
+ * ⁠   2 -> B
+ * ⁠   3 -> C
+ * ⁠   ...
+ * ⁠   26 -> Z
+ * ⁠   27 -> AA
+ * ⁠   28 -> AB 
+ * ⁠   ...
+ * 
+ * 
+ * 示例 1:
+ * 
+ * 输入: 1
+ * 输出: "A"
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: 28
+ * 输出: "AB"
+ * 
+ * 
+ * 示例 3:
+ * 
+ * 输入: 701
+ * 输出: "ZY"
+ * 
+ * 
+ */
+class Solution {
+
+    private char[] arr = new char[]{'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y'};
+
+    public String convertToTitle(int n) {
+        String r = "";
+        while (n > 0) {
+            int tmp = n % 26;
+            r = arr[tmp] + r;
+            if (tmp == 0) {
+                n = (n - 26) / 26;
+            } else {
+                n = (n - tmp) / 26;
+            }
+        }
+        return r;
+    }
+}
+
