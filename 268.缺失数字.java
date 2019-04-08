2a1ef2b01d5eee06fb026ab06a00d83b1380becb
ifelse0126 <ifelse0126@gmail.com>
2019-04-08 22:48:58 +0800

    test

---
+++ 268.缺失数字.java
@@ -0,0 +1,68 @@
+import java.util.ArrayList;
+import java.util.BitSet;
+
+/*
+ * @lc app=leetcode.cn id=268 lang=java
+ *
+ * [268] 缺失数字
+ *
+ * https://leetcode-cn.com/problems/missing-number/description/
+ *
+ * algorithms
+ * Easy (49.20%)
+ * Total Accepted:    17K
+ * Total Submissions: 34.5K
+ * Testcase Example:  '[3,0,1]'
+ *
+ * 给定一个包含 0, 1, 2, ..., n 中 n 个数的序列，找出 0 .. n 中没有出现在序列中的那个数。
+ * 
+ * 示例 1:
+ * 
+ * 输入: [3,0,1]
+ * 输出: 2
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: [9,6,4,2,3,5,7,0,1]
+ * 输出: 8
+ * 
+ * 
+ * 说明:
+ * 你的算法应具有线性时间复杂度。你能否仅使用额外常数空间来实现?
+ * 
+ */
+import java.util.*;
+
+
+class Solution {
+
+    public int missingNumber(int[] nums) { //binary search
+        Arrays.sort(nums);
+        int left = 0, right = nums.length, mid= (left + right)/2;
+        while(left<right){
+            mid = (left + right)/2;
+            if(nums[mid]>mid) right = mid;
+            else left = mid+1;
+        }
+        return left;
+    }
+
+    public int missingNumber3(int[] nums) { //xor
+        int res = nums.length;
+        for(int i=0; i<nums.length; i++){
+            res ^= i;
+            res ^= nums[i];
+        }
+        return res;
+    }
+
+    public int missingNumber2(int[] nums) {
+        int sum = (0 + nums.length) * (nums.length + 1) / 2;
+        for(int n : nums) {
+            sum = sum - n;
+        }
+        return sum;
+    }
+}
+
