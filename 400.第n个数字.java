4a749261854825cad59922a1f64634096100c106
ifelse0126 <ifelse0126@gmail.com>
2019-04-14 10:17:28 +0800

    放弃了

--- 400.第n个数字.java
+++ 400.第n个数字.java
@@ -41,7 +41,40 @@
  * 
  */
 class Solution {
+
     public int findNthDigit(int n) {
+        // digit表示要找的个位数“所属的”多位数x的位数
+        // t表示该多位数一共有多少个
+        // digit=1 t=9  表示1~9    中全是1位数，共有9个
+        // digit=2 t=90 表示10~99  中全是2位数，共有90个
+        // digit=3 t=900表示100~999中全是3位数，共有900个
+        int digit = 1;
+        Long t = 9L;
+        
+        // 找到n所求的数“所属的”多位数x的范围
+        while (n - t * digit > 0) {
+            n = (int) (n - t * digit);
+            t = t * 10;
+            digit++;
+        }
+        
+        // 此时应从位数为digit的数字里面找第n个，即为所求值
+        // basevalue是第一个被找的数字，比如1,10,100,1000...
+        // no_basevalue是x的值
+        int basevalue = (int) Math.pow(10, digit-1);
+        int no_basevalue = (n - 1) / digit;
+        basevalue += no_basevalue;
+        // 找到所求的“个位数”在x中的下标，返回
+        // 此时n的意义是：在所有digit位数连接成的字符串中所求数的下标位置+1
+        // 下标位置+1，所以要n-1
+        // 所有digit位数的字符串中的下标，所以对digit取余数
+        char c = String.valueOf(basevalue).toCharArray()[(n - 1) % digit];
+        return Integer.parseInt("" + c);
+    }
+
+    
+    // 1000000000 time limit exception
+    public int findNthDigit2(int n) {
         long num = n;
         long segment = 9;
         int start = 1;
@@ -60,5 +93,3 @@ class Solution {
             : String.valueOf(step + count).charAt(i - 1) - '0';
     }
 }
-
-// 1000000000 time limit exception
