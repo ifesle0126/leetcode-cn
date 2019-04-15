7fae94a8587b1b1f284e4ee3b7289398bc86df6c
zhujq <zhujq@ifeng.com>
2019-04-15 11:37:19 +0800

    bug fixed

--- 405.数字转换为十六进制数.java
+++ 405.数字转换为十六进制数.java
@@ -49,6 +49,9 @@ import com.sun.org.apache.xpath.internal.SourceTree;
 class Solution {
     char[] chars = new char[]{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
     public String toHex(int num) {
+        if (num == 0) {
+            return "0";
+        }
         String r = "";
         int times = 0;
         while (num != 0 && times < 8) {
