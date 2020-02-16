0748f770644e6fe0875572f0964ed49458917036
zhujianqiang <zhujianqiang@58.com>
2020-02-16 18:51:46 +0800

    solution

--- 45.跳跃游戏-ii.java
+++ 45.跳跃游戏-ii.java
@@ -46,8 +46,8 @@ class Solution {
         int sc = 0;
         int e = 0;
         int max = 0;
-        for(int i=0; i < nums.length-1; i++) {
-            max = Math.max(max, i+nums[i]);
+        for(int i=0; i < nums.length - 1; i++) {
+            max = Math.max(max, i + nums[i]);
             if( i == e ) {
                 sc++;
                 e = max;
