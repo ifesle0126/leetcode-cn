549f414051d36028d4e04dbc936a0f15c35a6c27
zhujq <ifelse0126@gmail.com>
2020-03-01 16:59:09 +0800

    solution

---
+++ 81.搜索旋转排序数组-ii.java
@@ -0,0 +1,76 @@
+/*
+ * @lc app=leetcode.cn id=81 lang=java
+ *
+ * [81] 搜索旋转排序数组 II
+ *
+ * https://leetcode-cn.com/problems/search-in-rotated-sorted-array-ii/description/
+ *
+ * algorithms
+ * Medium (35.15%)
+ * Likes:    110
+ * Dislikes: 0
+ * Total Accepted:    19.2K
+ * Total Submissions: 54.5K
+ * Testcase Example:  '[2,5,6,0,0,1,2]\n0'
+ *
+ * 假设按照升序排序的数组在预先未知的某个点上进行了旋转。
+ * 
+ * ( 例如，数组 [0,0,1,2,2,5,6] 可能变为 [2,5,6,0,0,1,2] )。
+ * 
+ * 编写一个函数来判断给定的目标值是否存在于数组中。若存在返回 true，否则返回 false。
+ * 
+ * 示例 1:
+ * 
+ * 输入: nums = [2,5,6,0,0,1,2], target = 0
+ * 输出: true
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: nums = [2,5,6,0,0,1,2], target = 3
+ * 输出: false
+ * 
+ * 进阶:
+ * 
+ * 
+ * 这是 搜索旋转排序数组 的延伸题目，本题中的 nums  可能包含重复元素。
+ * 这会影响到程序的时间复杂度吗？会有怎样的影响，为什么？
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int start = 0, end = nums.length - 1, mid = -1;
+        while(start <= end) {
+            mid = (start + end) / 2;
+            if (nums[mid] == target) {
+                return true;
+            }
+            //If we know for sure right side is sorted or left side is unsorted
+            if (nums[mid] < nums[end] || nums[mid] < nums[start]) {
+                if (target > nums[mid] && target <= nums[end]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            //If we know for sure left side is sorted or right side is unsorted
+            } else if (nums[mid] > nums[start] || nums[mid] > nums[end]) {
+                if (target < nums[mid] && target >= nums[start]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            //If we get here, that means nums[start] == nums[mid] == nums[end], then shifting out
+            //any of the two sides won't change the result but can help remove duplicate from
+            //consideration, here we just use end-- but left++ works too
+            } else {
+                end--;
+            }
+        }
+        return false;
+    }
+}
+// @lc code=end
+
