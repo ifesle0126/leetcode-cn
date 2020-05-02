b09c5d79b963ad310b04407b59c8257a7257b2a8
zhujq <ifelse0126@gmail.com>
2020-05-02 18:29:48 +0800

    solution

---
+++ 173.二叉搜索树迭代器.java
@@ -0,0 +1,125 @@
+/*
+ * @lc app=leetcode.cn id=173 lang=java
+ *
+ * [173] 二叉搜索树迭代器
+ *
+ * https://leetcode-cn.com/problems/binary-search-tree-iterator/description/
+ *
+ * algorithms
+ * Medium (72.59%)
+ * Likes:    168
+ * Dislikes: 0
+ * Total Accepted:    18K
+ * Total Submissions: 24.8K
+ * Testcase Example:  '["BSTIterator","next","next","hasNext","next","hasNext","next","hasNext","next","hasNext"]\n' +
+  '[[[7,3,15,null,null,9,20]],[null],[null],[null],[null],[null],[null],[null],[null],[null]]'
+ *
+ * 实现一个二叉搜索树迭代器。你将使用二叉搜索树的根节点初始化迭代器。
+ * 
+ * 调用 next() 将返回二叉搜索树中的下一个最小的数。
+ * 
+ * 
+ * 
+ * 示例：
+ * 
+ * 
+ * 
+ * BSTIterator iterator = new BSTIterator(root);
+ * iterator.next();    // 返回 3
+ * iterator.next();    // 返回 7
+ * iterator.hasNext(); // 返回 true
+ * iterator.next();    // 返回 9
+ * iterator.hasNext(); // 返回 true
+ * iterator.next();    // 返回 15
+ * iterator.hasNext(); // 返回 true
+ * iterator.next();    // 返回 20
+ * iterator.hasNext(); // 返回 false
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * next() 和 hasNext() 操作的时间复杂度是 O(1)，并使用 O(h) 内存，其中 h 是树的高度。
+ * 你可以假设 next() 调用总是有效的，也就是说，当调用 next() 时，BST 中至少存在一个下一个最小的数。
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+import java.util.*;
+
+
+class BSTIterator {
+    private Stack<TreeNode> stack = new Stack<TreeNode>();
+    
+    public BSTIterator(TreeNode root) {
+        pushAll(root);
+    }
+
+    /** @return whether we have a next smallest number */
+    public boolean hasNext() {
+        return !stack.isEmpty();
+    }
+
+    /** @return the next smallest number */
+    public int next() {
+        TreeNode tmpNode = stack.pop();
+        pushAll(tmpNode.right);
+        return tmpNode.val;
+    }
+    
+    private void pushAll(TreeNode node) {
+        for (; node != null; stack.push(node), node = node.left);
+    }
+}
+
+class BSTIterator2 {
+
+    private List<Integer> sortedList = null;
+
+    private int index;
+
+    public BSTIterator2(TreeNode root) {
+        this.sortedList = new ArrayList<>();
+        this.index = 0;
+        sortBST(root);
+    }
+
+    private void sortBST(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        sortBST(node.left);
+        sortedList.add(node.val);
+        sortBST(node.right);
+    }
+    
+    /** @return the next smallest number */
+    public int next() {
+        return sortedList.get(index++);
+    }
+    
+    /** @return whether we have a next smallest number */
+    public boolean hasNext() {
+        return index < sortedList.size();
+    }
+}
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator obj = new BSTIterator(root);
+ * int param_1 = obj.next();
+ * boolean param_2 = obj.hasNext();
+ */
+// @lc code=end
+
