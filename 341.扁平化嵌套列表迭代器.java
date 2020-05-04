842e95ee604b7f3da3a5823cd13a9d0cfbbb9bcc
zhujq <ifelse0126@gmail.com>
2020-05-04 10:52:20 +0800

    solution

---
+++ 341.扁平化嵌套列表迭代器.java
@@ -0,0 +1,93 @@
+/*
+ * @lc app=leetcode.cn id=341 lang=java
+ *
+ * [341] 扁平化嵌套列表迭代器
+ *
+ * https://leetcode-cn.com/problems/flatten-nested-list-iterator/description/
+ *
+ * algorithms
+ * Medium (63.10%)
+ * Likes:    88
+ * Dislikes: 0
+ * Total Accepted:    7.8K
+ * Total Submissions: 12.4K
+ * Testcase Example:  '[[1,1],2,[1,1]]'
+ *
+ * 给你一个嵌套的整型列表。请你设计一个迭代器，使其能够遍历这个整型列表中的所有整数。
+ * 
+ * 列表中的每一项或者为一个整数，或者是另一个列表。其中列表的元素也可能是整数或是其他列表。
+ * 
+ * 
+ * 
+ * 示例 1:
+ * 
+ * 输入: [[1,1],2,[1,1]]
+ * 输出: [1,1,2,1,1]
+ * 解释: 通过重复调用 next 直到 hasNext 返回 false，next 返回的元素的顺序应该是: [1,1,2,1,1]。
+ * 
+ * 示例 2:
+ * 
+ * 输入: [1,[4,[6]]]
+ * 输出: [1,4,6]
+ * 解释: 通过重复调用 next 直到 hasNext 返回 false，next 返回的元素的顺序应该是: [1,4,6]。
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * public interface NestedInteger {
+ *
+ *     // @return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     public boolean isInteger();
+ *
+ *     // @return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // Return null if this NestedInteger holds a nested list
+ *     public Integer getInteger();
+ *
+ *     // @return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // Return null if this NestedInteger holds a single integer
+ *     public List<NestedInteger> getList();
+ * }
+ */
+import java.util.*;
+
+public class NestedIterator implements Iterator<Integer> {
+    Deque<NestedInteger> stack = new ArrayDeque<>();
+    public NestedIterator(List<NestedInteger> nestedList) {
+        prepareStack(nestedList);
+    }
+
+    @Override
+    public Integer next() {
+        if (!hasNext()) {
+            return null;
+        }
+        return stack.pop().getInteger();
+    }
+
+    @Override
+    public boolean hasNext() {
+        while (!stack.isEmpty() && !stack.peek().isInteger()) {
+            List<NestedInteger> list = stack.pop().getList();
+            prepareStack(list);
+        }
+        return !stack.isEmpty();
+    }
+    
+    private void prepareStack(List<NestedInteger> nestedList) {
+        for (int i = nestedList.size() - 1; i >= 0; i--) {
+            stack.push(nestedList.get(i));
+        }
+    }
+}
+
+/**
+ * Your NestedIterator object will be instantiated and called as such:
+ * NestedIterator i = new NestedIterator(nestedList);
+ * while (i.hasNext()) v[f()] = i.next();
+ */
+// @lc code=end
+
---
+++ 432.全-o-1-的数据结构.java
@@ -0,0 +1,171 @@
+/*
+ * @lc app=leetcode.cn id=432 lang=java
+ *
+ * [432] 全 O(1) 的数据结构
+ *
+ * https://leetcode-cn.com/problems/all-oone-data-structure/description/
+ *
+ * algorithms
+ * Hard (34.28%)
+ * Likes:    43
+ * Dislikes: 0
+ * Total Accepted:    2.8K
+ * Total Submissions: 8.3K
+ * Testcase Example:  '["AllOne","getMaxKey","getMinKey"]\n[[],[],[]]'
+ *
+ * 请你实现一个数据结构支持以下操作：
+ * 
+ * 
+ * Inc(key) - 插入一个新的值为 1 的 key。或者使一个存在的 key 增加一，保证 key 不为空字符串。
+ * Dec(key) - 如果这个 key 的值是 1，那么把他从数据结构中移除掉。否则使一个存在的 key 值减一。如果这个 key
+ * 不存在，这个函数不做任何事情。key 保证不为空字符串。
+ * GetMaxKey() - 返回 key 中值最大的任意一个。如果没有元素存在，返回一个空字符串"" 。
+ * GetMinKey() - 返回 key 中值最小的任意一个。如果没有元素存在，返回一个空字符串""。
+ * 
+ * 
+ * 
+ * 
+ * 挑战：
+ * 
+ * 你能够以 O(1) 的时间复杂度实现所有操作吗？
+ * 
+ */
+
+// @lc code=start
+import java.util.*;
+
+public class AllOne {
+
+    // Map that stores key to the node
+    Map<String, Integer> map;
+    
+    // Map that groups keys having same value using double
+    // linked list as value
+    Map<Integer, Set<String>> valueMap;
+    
+    // MinMax linkedlist to keep track of
+    // min and max values
+    LinkedList<Integer> minMax;
+
+    /** Initialize your data structure here. */
+    public AllOne() {
+        map = new HashMap<>();
+        valueMap = new HashMap<>();
+        minMax = new LinkedList<Integer>();
+    }
+    
+    /** Inserts a new key <Key> with value 1. Or increments an existing key by 1. */
+    public void inc(String key) {
+        // If new key, just insert into the map
+        // and place it in the appropriate position
+        // in value map
+        if(!map.containsKey(key)) {
+            map.put(key, 1);
+            putInValueMap(1, key);
+        } else {
+            // If already existing
+            int val = map.get(key);
+            // Remove from value map for old value
+            removeFromValueMap(val, key);
+            // Increment value
+            map.put(key, val + 1);
+            // Place it the new value in value map
+            putInValueMap(val + 1, key);
+        }
+    }
+    
+    /** Decrements an existing key by 1. If Key's value is 1, remove it from the data structure. */
+    public void dec(String key) {
+        // If this key not found, just return
+        if(!map.containsKey(key)) {
+            return;
+        }
+        
+        int val = map.get(key);
+        // If value is 1, then remove from map
+        if(val == 1) {
+            map.remove(key);
+            // Remove from value map too
+            removeFromValueMap(1, key);
+        } else {
+            // Remove from old value
+            removeFromValueMap(val, key);
+            // Decrement value
+            map.put(key, val - 1);
+            
+            // Insert at new value
+            putInValueMap(val - 1, key);
+        }
+        
+    }
+    
+    /** Returns one of the keys with maximal value. */
+    public String getMaxKey() {
+        if(minMax.isEmpty()) {
+            return "";
+        }
+        // minMax.getFirst() always has max value
+        return valueMap.get(minMax.getFirst()).iterator().next();
+    }
+    
+    /** Returns one of the keys with Minimal value. */
+    public String getMinKey() {
+        if(minMax.isEmpty()) {
+            return "";
+        }
+        // minMax.getFirst() always has min value
+        return valueMap.get(minMax.getLast()).iterator().next();
+    }
+    
+    private void putInValueMap(int count, String node) {
+        // If not seen before, create a new dll
+        if(!valueMap.containsKey(count)) {
+            valueMap.put(count, new HashSet<String>());
+        }
+        // Add to dll
+        valueMap.get(count).add(node);
+        
+        // Update min max
+        if(minMax.isEmpty() || minMax.getFirst() < count) {
+            minMax.addFirst(count);
+        }
+        
+        if(!minMax.isEmpty() && minMax.getLast() > count) {
+            minMax.addLast(count);
+        }
+    }
+    
+    private void removeFromValueMap(int count, String node) {
+        // If not present in value map, just return
+        if(!valueMap.containsKey(count)) {
+            return;
+        }
+        
+        // Remove from dll in value map
+        valueMap.get(count).remove(node);
+        
+        // If no elements present with this value
+        if(valueMap.get(count).size() == 0) {
+            // Remove from value map all together
+            valueMap.remove(count);
+            
+            // Update min max
+            if(!minMax.isEmpty() && minMax.getFirst() == count) {
+                minMax.removeFirst();
+            }
+            if(!minMax.isEmpty() && minMax.getLast() == count) {
+                minMax.removeLast();
+            }
+        }
+    }
+}
+/**
+ * Your AllOne object will be instantiated and called as such:
+ * AllOne obj = new AllOne();
+ * obj.inc(key);
+ * obj.dec(key);
+ * String param_3 = obj.getMaxKey();
+ * String param_4 = obj.getMinKey();
+ */
+// @lc code=end
+
