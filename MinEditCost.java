532150b922207fbbe38cc62c38093546b56fa3c8
zhujq <ifelse0126@gmail.com>
2021-04-18 19:38:16 +0800

    question description

--- MinEditCost.java
+++ MinEditCost.java
@@ -1,5 +1,11 @@
 import java.util.*;
 
+/**
+ * 给定两个字符串str1和str2，再给定三个整数ic，dc和rc，分别代表插入、删除和替换一个字符的代价，请输出将str1编辑成str2的最小代价
+ * 
+ * 输入："abc","adc",5,3,2
+ * 返回：2
+ */
 public class MinEditCost {
     
     public static void main(String[] args) {
