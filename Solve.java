640ab060a45c40e033620e370ff8c8f4b065ea13
zhujq <ifelse0126@gmail.com>
2021-06-19 20:21:45 +0800

    feature

---
+++ Solve.java
@@ -0,0 +1,64 @@
+import java.util.*;
+
+
+public class Solve {
+
+    public static void main(String[] args) {
+        Solve solve = new Solve();
+        // String s = "(1+((2+3)+4+5)+6)";
+        String s = "((10+2)*10-(100-(10+20*10-(2*3)))*10*1*2)-2";
+        int num = solve.solve(s);
+        System.out.println(num);
+    }
+    
+    public int solve(String s) {
+        s = s.trim();
+        Deque<Integer> stack = new ArrayDeque<>();
+        int number = 0;
+        char sign = '+';
+        char[] charArray = s.toCharArray();
+        for (int i = 0, n = charArray.length; i < n; i++) {
+            char c = charArray[i];
+            if (c == ' ') {
+                continue;
+            }
+            if (Character.isDigit(c)) {
+                number = number * 10 + c - '0';
+            }
+            if (c == '(') {
+                int j = i + 1;
+                int counterPartition = 1;
+                while (counterPartition > 0) {
+                    if (charArray[j] == '(') {
+                        counterPartition++;
+                    }
+                    if (charArray[j] == ')') {
+                        counterPartition--;
+                    }
+                    j++;
+                }
+                number = solve(s.substring(i + 1, j - 1));
+                i = j - 1;
+            }
+            if (!Character.isDigit(c) || i == n - 1) {
+                if (sign == '+') {
+                    stack.push(number);
+                } else if (sign == '-') {
+                    stack.push(-1 * number);
+                } else if (sign == '*') {
+                    stack.push(stack.pop() * number);
+                } else if (sign == '/') {
+                    stack.push(stack.pop() / number);
+                }
+                number = 0;
+                sign = c;
+            }
+        }
+        int ans = 0;
+        while (!stack.isEmpty()) {
+            ans += stack.pop();
+        }
+        return ans;
+
+    }
+}
